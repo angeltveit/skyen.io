31a39014f2eb6700f1f9406d9ae9d26c6633b8ff
angeltveit <tan@scoutgg.com>
2024-04-23 21:20:29 +0200

    staging message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,5 @@
 <div class="m-w-dvw m-h-dvh flex flex-col items-center justify-center p-5">
   <div><img class="logo" src="/cloud.webp" alt="Cloud.lol" /></div>
   <div class="my-5 text-4xl font-bold">Last opp i skyen lol</div>
-
-  <div class="construction flex sm:block flex-col">
-    <img class="inline sign mr-5"  src="/construction-1.gif" />
-    <img class="inline mr-5"  src="/construction-2.gif" />
-    <img class="inline sign"  src="/construction-1.gif" />
-  </div>
+  <div class="uppercase"> This is the staging environment</div>
 </div>
